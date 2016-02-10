7abf863a44f96dee2803508c467cdf6b63684efd
Ahmed Abdel-Kader <ahmedqad7790@gmail.com>
2016-02-10 18:06:52 +0200

    Add Buzzer.h

---
+++ Buzzer.h
@@ -0,0 +1,27 @@
+﻿/*
+ * Buzzer.h
+ *
+ * Created: 05/04/2015 12:53:53 ص
+ *  Author: Ahmed Abdel-Kader
+ */ 
+
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+#include "Timer.h"
+#include "DIO.h"
+
+#define Buzzer_Dir OUTPUT
+#define Buzzer_Port 'C'
+#define Buzzer_Pin_Num 3
+
+static DIO_Config g_buzzer_config;
+
+void Buzzer_Config(void);
+
+void Buzzer_Start (void);
+
+	
+
+#endif /* BUZZER_H_ */
